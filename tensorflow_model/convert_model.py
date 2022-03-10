3f54c32045dd67e9b03c984777968f914da9bdec
jonahobw <jgow98@gmail.com>
2022-03-10 11:08:31 -0500

    convert to tflite

---
+++ tensorflow_model/convert_model.py
@@ -0,0 +1,19 @@
+from model import Residual_CNN
+import config
+from game import Game
+
+import tensorflow as tf
+
+env = Game()
+
+model = Residual_CNN(config.REG_CONST, config.LEARNING_RATE, env.input_shape, env.action_size,
+                              config.HIDDEN_CNN_LAYERS)
+
+model_network = model.read(env.name, run_number=0, version=32)
+model.model.set_weights(model_network.get_weights())
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model.model)
+tflite_model = converter.convert()
+
+with open('model.tflite', 'wb') as f:
+  f.write(tflite_model)
